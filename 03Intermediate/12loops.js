b35683ec1c191ddd97676f4226dd628b02c354a1
JimHui12 <jianghua.hui@uq.net.au>
2021-02-25 18:05:55 +1000

    loops added

--- 03Intermediate/12loops.js
+++ 03Intermediate/12loops.js
@@ -4,8 +4,18 @@
 
 const myState = ["Taiwan", "Guangdong", 2021, "Shandong", "Guangxi", "Fujian", "Hunan"];
 
-for (let i = 0; i < myState.length; i++) {
-    if(typeof myState[i] !== 'string') break;
+// for (let i = 0; i < myState.length; i++) {
+//     if(typeof myState[i] !== 'string') break;
 
-    console.log(myState[i]);
-}
+//     console.log(myState[i]);
+// }
+
+let i = 20;
+
+do {
+    console.log([i]);
+    i++;
+} while(i < 10); 
+
+
+
