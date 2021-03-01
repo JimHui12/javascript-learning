cfa5006a587ae588f93b685933e08a030f6b8fc4
JimHui12 <jianghua.hui@uq.net.au>
2021-03-01 16:10:31 +1000

    Object.js updated

--- 07NotAdvance/01Objects.js
+++ 07NotAdvance/01Objects.js
@@ -1,11 +1,25 @@
 var User = function(firstName, courseCount) {
-    this.firstName = firstName;
+    this.firstName = firstName; // this point window/global object change to 
     this.courseCount = courseCount;
     this.getCoureCount = function() {
         console.log(`Course count is: ${this.courseCount}`);
     };
 };
 
-var myname = User("myname", 2);
+User.prototype.getFirstName = function() {
+    console.log(`Your first name is : ${this.firstName}`);
+}
 
-console.log(myname);
+var myname = new User("myname", 2); // not a regular 
+myname.getCoureCount();
+
+if (myname.hasOwnProperty("firstName")) {
+    myname.getFirstName();
+}
+
+//console.log(myname);
+
+var sam = new User("Sam", 1);
+//console.log(sam);
+sam.getCoureCount();
+sam.getFirstName();
