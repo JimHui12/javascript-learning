240c80c5ef0dd35b3fc9c7444e5b77d06376f680
JimHui12 <jianghua.hui@uq.net.au>
2021-03-02 18:19:41 +1000

    scrope.js added

---
+++ 09Advanced/01scope.js
@@ -0,0 +1,12 @@
+// console.log(name);
+var name = "hi";
+
+if (true) {
+    var lastName = "yes";
+    console.log(lastName);
+}
+
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+    
+// }
