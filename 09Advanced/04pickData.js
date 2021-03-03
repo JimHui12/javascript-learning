eeee3274ec53427e91e80044442f7af431f99005
JimHui12 <jianghua.hui@uq.net.au>
2021-03-03 10:35:51 +1000

    spread.js added

--- 09Advanced/04pickData.js
+++ 09Advanced/04pickData.js
@@ -1,8 +1,18 @@
-const user = ["hi", 3, "admin"];
-// var role = user[2];
-// var name = user[0];
+// const user = ["hi", 3, "admin"];
+// // var role = user[2];
+// // var name = user[0];
 
-var [name, courseCount, role] = user;
+// var [name, courseCount, role] = user;
 
+// console.log(role);
 
+const MyUser = {
+    name: "hi",
+    courseCount: 5,
+    role: "admin",
+};
+
+console.log(MyUser.courseCount);
+
+const {name, courseCount, role} = MyUser;
 console.log(role);
